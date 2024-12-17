2094ead52851d6c1f45074ea8d52e6a73c03bff1
Aly-Sall <alysall2001@gmail.com>
2024-12-17 22:49:52 +0100

    finaished the authentification

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -13,7 +13,41 @@ const Navbar = async () => {
           <Image src="/logo.png" alt="Logo" width={144} height={30} />
         </Link>
 
-        <div className="flex items-center gap-5"></div>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({
+                    redirectTo: "/",
+                  });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
